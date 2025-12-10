373200a8d9afdfb674140f510042b86f75f7efb5
22300941 <a22300941@ceti.mx>
2025-12-10 08:12:29 -0600

    Add files via upload
    cliente y proveedor

---
+++ WPFProyecto-PAI/BD/Tablas/cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WPFProyecto_PAI.Tablas
+{
+    public class cliente
+    {
+        public int id_cliente { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+        public string firma { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int id_sucursal { get; set; }   // FK hacia sucursal
+    }
+}
---
+++ WPFProyecto-PAI/BD/Tablas/proveedores.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WPFProyecto_PAI.Tablas
+{
+    public class proveedor
+    {
+        public int id_proveedor { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+    }
+}
